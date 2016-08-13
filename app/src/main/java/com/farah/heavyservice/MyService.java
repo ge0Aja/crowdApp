40bc5148fea819c258f8d1bb1f6ebbc29addcf6c
Georgi <geeorgyaja@gmail.com>
2016-08-13 17:05:20 +0300

    Results Receiver from the Sync Service

--- app/src/main/java/com/farah/heavyservice/MyService.java
+++ app/src/main/java/com/farah/heavyservice/MyService.java
@@ -260,6 +260,8 @@ public class MyService extends Service
                         appendLog(messageLogged); Log.i("Traffic", messageLogged);
                     }
                     // Here We Should be Adding the Reulst Recevier and Starting the Serivce
+
+                  ///// Start You can comment this section out and keep working fine
                     HttpResultsReceiver mRec = new HttpResultsReceiver(new android.os.Handler());
                     Intent intent = new Intent(Intent.ACTION_SYNC,null,getApplicationContext(),ClientServerService.class);
                     intent.putExtra("url","The URL on the Server");
@@ -267,7 +269,7 @@ public class MyService extends Service
                     intent.putExtra("Method","POST");
                     intent.putExtra("HashMap",outerHash); // I'm not sure wetehr to send the List or the Outer Hash every 10 Secs
                     startService(intent);
-
+                    ///// End You can comment this section out and keep working fine
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
